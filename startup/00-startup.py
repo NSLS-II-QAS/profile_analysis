e362427fcf3f9eaf851e7255374b007de8bff5d4
Julien Lhermitte <lhermitte@bnl.gov>
2018-06-22 13:58:33 -0400

    Added xview()

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,18 +1,20 @@
 import databroker
 from PyQt5.QtWidgets import QApplication
 import sys
-from isstools import Xview
+from isstools.xview import XviewGui
 
 import requests
 #from requests.packages.urllib3.exceptions import InsecureRequestWarning
 #requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-db = databroker.Broker.named('iss')
+db = databroker.Broker.named('qas')
 
 # TODO : we shouldn't need this
-qas_mono_pulses_per_deg= 10000
+QAS_MONO_PULSES_PER_DEGREE = 23600*400/360
 app = QApplication(sys.argv)
-xview_gui = Xview.XviewGui(iss_mono_pulses_per_deg, db=db)
+xview_gui = XviewGui(QAS_MONO_PULSES_PER_DEGREE, db=db)
 
 def xview():
     xview_gui.show()
+
+xview()
